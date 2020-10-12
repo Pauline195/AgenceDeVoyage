16221f3d348fd051479e12638083a92c305ace5e
hugohavez <arby.havez@gmail.com>
2020-10-12 16:14:33 +0200

    dao

---
+++ src/com/inti/dao/impl/ReservationDAO.java
@@ -0,0 +1,8 @@
+package com.inti.dao.impl;
+
+import com.inti.dao.interfaces.IReservationDAO;
+import com.inti.entities.Reservation;
+
+public class ReservationDAO extends ManagerDAO<Reservation> implements IReservationDAO {
+
+}
